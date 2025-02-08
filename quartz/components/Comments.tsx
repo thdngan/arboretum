3200cdde35a01123469cfe62458b07dbeca65caa
thdngan <trinhhoangdieungan@gmail.com>
2025-02-09 00:53:12 +0700

    Quartz sync: Feb 9, 2025, 12:53 AM

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -25,15 +25,14 @@ function boolToStringBool(b: boolean): string {
 }
 
 export default ((opts: Options) => {
-  // const Comments: QuartzComponent = ({ displayClass, fileData, cfg }: QuartzComponentProps) => {
+  const Comments: QuartzComponent = ({ displayClass, fileData, cfg }: QuartzComponentProps) => {
     // check if comments should be displayed according to frontmatter
-    const Comments: QuartzComponent = ({ displayClass, fileData, cfg }: QuartzComponentProps) => {
-      // check if comments should be displayed according to frontmatter
-      const commentsFlag: boolean =
-        fileData.frontmatter?.comments === true || fileData.frontmatter?.comments === "true"
-      if (!commentsFlag) {
-        return <></>
-      }
+    const disableComment: boolean =
+      typeof fileData.frontmatter?.comments !== "undefined" &&
+      (!fileData.frontmatter?.comments || fileData.frontmatter?.comments === "false")
+    if (disableComment) {
+      return <></>
+    }
     return (
       <div
         class={classNames(displayClass, "giscus")}
