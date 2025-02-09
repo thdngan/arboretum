0fc2c940d5bf6db85a09628369b7fa9f965ab7cd
thdngan <trinhhoangdieungan@gmail.com>
2025-02-09 23:46:46 +0700

    Quartz sync: Feb 9, 2025, 11:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "L'arboretum", /**🐧 */
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
