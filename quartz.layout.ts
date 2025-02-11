88f862fb99a29c8e37ba5d912b24075b7798cdcb
thdngan <trinhhoangdieungan@gmail.com>
2025-02-11 11:47:49 +0700

    Quartz sync: Feb 11, 2025, 11:47 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  // afterBody: [],
   afterBody: [
     Component.Comments({
       provider: 'giscus',
