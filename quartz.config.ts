f6e939aecc2c06664fcfb33b3d1e4bb878b06b58
thdngan <trinhhoangdieungan@gmail.com>
2025-02-15 21:23:00 +0700

    Quartz sync: Feb 15, 2025, 9:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "JetBrains", /** Schibsted Grotesk, Chelsea Market,Libre Baskerville*/
-        body: "Inter", /**Source Sans Pro, Roboto Serif */
+        body: "Bricolage Grotesque", /**Source Sans Pro, Roboto Serif */
         code: "JetBrains Mono", /**IBM Plex Mono */
       },
       colors: {
