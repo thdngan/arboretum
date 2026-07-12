787dc18cd36af321490417a37d07968fe42f6514
thdngan <trinhhoangdieungan@gmail.com>
2026-07-12 17:10:18 +0200

    Quartz sync: Jul 12, 2026, 5:10 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -30,7 +30,11 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       const segments: (string | JSX.Element)[] = []
 
       if (fileData.dates) {
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
+        segments.push(
+          <span>
+            Created on <Date date={getDate(cfg, fileData)!} locale={cfg.locale} />
+          </span>
+        )
       }
 
       // Display reading time if enabled
